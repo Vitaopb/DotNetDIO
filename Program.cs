39511d319eca392a4504b9f693f1b8a4a23e2d1d
Vitaopb <victor.lieuthier@gmail.com>
2021-09-08 01:41:06 -0300

    Primeiro programa

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DotNetDIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 5;
+            for (int i = 1; i <= cont; i++) {
+                Console.WriteLine($"{i} Repetição");
+            }
+        }
+    }
+}
