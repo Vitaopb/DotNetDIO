1a1b9db48d73f2edc07e88cb9b19be8ed8d9f6af
Vitaopb <victor.lieuthier@gmail.com>
2021-09-09 14:36:59 -0300

    aula 2

--- Aula-02/Program.cs
+++ Aula-02/Program.cs
@@ -6,7 +6,13 @@ namespace Aula_02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int[] array = new int[10]; "Já detertermino a length do meu array, mas não os dados"
+            int[] array = {1, 2, 3, 4, 5};
+            for (int i = 0; i < array.Length; i++) 
+            {
+                array[i] = i * i;
+                Console.WriteLine($"array[{i}] = {array[i]}");
+            }
         }
     }
 }
